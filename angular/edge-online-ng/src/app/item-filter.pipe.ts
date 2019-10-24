0bf83f4248905932251c78b6972e85956de8ccf8
Lee Angioletti <langio@outlook.com>
2019-10-24 15:17:57 -0400

    finished catalog filtering

--- angular/edge-online-ng/src/app/item-filter.pipe.ts
+++ angular/edge-online-ng/src/app/item-filter.pipe.ts
@@ -1,12 +1,29 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { Item } from './model/item.model';
 
 @Pipe({
   name: 'itemFilter'
 })
 export class ItemFilterPipe implements PipeTransform {
 
-  transform(items: any, itemNameFilter: string): any {
+  transform(items: any, itemNameFilter: string, stockOnly: boolean, providerFilter: string, categoryFilter: string): any {
+
+    if (stockOnly) {
+      items = items.filter(i => {
+        return i.stock > 0;
+      });
+    }
+    if (providerFilter != "") {
+      items = items.filter(i => {
+        return i.provider.name === providerFilter;
+      });
+    }
+
+    if (categoryFilter != "") {
+      items = items.filter(i => {
+        return i.category === categoryFilter;
+      });
+    }
+
     if (itemNameFilter == "") {
       return items;
     }
